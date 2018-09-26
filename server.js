c96ad8c053b8f4b63b2ffeaeaf4af14e739216b4
Jarek Kajko <jaruto7@gmail.com>
2018-09-26 20:18:24 +0200

    Comment you code

--- server.js
+++ server.js
@@ -4,25 +4,32 @@ var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
 var app = express();
 var stringyFile;
-
+// Uzyj metody aby zaczac serwowac pliki w danym katalogu
 app.use( express.static( 'assets' ) );
-
+// Wywolaj metode GET  
 app.get( '/', function( req, res ) {
+    // Wywolaj metode aby wyslala w odpowiedzi plik zamiast wiadomosc
     res.sendFile( '/index.html' );
 });
-
+// Wywolaj metode GET jesli endpoint zostanie spelniony
 app.get( '/userform', function( req, res ) {
+    // Utworz stala zmienna ktora przechowa obiekty
     const response = {
+        // Przypisz do klucza parametr zadania zapytania do nowych obiektow
         first_name: req.query.first_name,
         last_name: req.query.last_name
     };
+    // Uzyj metody aby wyswietlic obiekt i za pomoca metody JSON.stringify przetworz ten
+    // obiekt response na string
     res.end( JSON.stringify( response ) );
 });
 
+// Nasluchuj na port i adres url serwera
 var server = app.listen( 3000, 'localhost', function() {
+    // Pobierz dane za pomoca metody adress i zapisz w zmiennych 
     var host = server.address().address;
     var port = server.address().port;
-
+    // Wyswietl komunikat adresu url serwera
     console.log( 'Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port );
 });
 
