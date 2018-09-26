35788983b653a747594610fe9236fbc5806bf903
Jarek Kajko <jaruto7@gmail.com>
2018-09-26 19:54:50 +0200

    Create Form & download data to the server

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ var stringyFile;
 
 app.use( express.static( 'assets' ) );
 
-app.get( '/', function(req, res ) {
-    res.send( 'Hello world' );
+app.get( '/', function( req, res ) {
+    res.sendFile( '/index.html' );
+});
+
+app.get( '/userform', function( req, res ) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end( JSON.stringify( response ) );
 });
 
 var server = app.listen( 3000, 'localhost', function() {
